1a321061cba90997f5a35dfa7ad73bd9143ffdc3
Leah <lobrien20@qub.ac.uk>
2023-07-12 14:42:44 +0100

    Fixing conda -> mamba switch in binning.py

--- binning.py
+++ binning.py
@@ -14,7 +14,7 @@ class MetaSpadesAssemblyRunner:
     
     def run_assembly(self):
         metaspades_result_directory = f"{self.output_directory}/metaspades_assembly_directory/"
-        metaspades_assembly_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/prebinning', 'spades.py', '-1', self.read_fwd_path, '-2', self.read_rev_path, '-t', self.threads, '--meta', '-o', metaspades_result_directory]
+        metaspades_assembly_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/prebinning', 'spades.py', '-1', self.read_fwd_path, '-2', self.read_rev_path, '-t', self.threads, '--meta', '-o', metaspades_result_directory]
         run_and_log_a_subprocess(self.output_directory, metaspades_assembly_args, "metaspades_assembly_auto_kmer")
 
         if self.using_scaffolds == True:
@@ -29,7 +29,7 @@ class MetaSpadesAssemblyRunner:
 
     def run_assembly_with_custom_kmer_lengths(self, custom_kmer_lengths):
         metaspades_result_directory = f"{self.output_directory}/metaspades_assembly_directory/"
-        metaspades_assembly_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/prebinning', 'spades.py', '-1', self.read_fwd_path, '-2', self.read_rev_path, '-t', self.threads, '-k', custom_kmer_lengths, '--meta', '-o', metaspades_result_directory]
+        metaspades_assembly_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/prebinning', 'spades.py', '-1', self.read_fwd_path, '-2', self.read_rev_path, '-t', self.threads, '-k', custom_kmer_lengths, '--meta', '-o', metaspades_result_directory]
         run_and_log_a_subprocess(self.output_directory, metaspades_assembly_args, "metaspades_assembly_auto_kmer")
 
         if self.using_scaffolds == True:
@@ -46,7 +46,7 @@ class MetaSpadesAssemblyRunner:
     def check_if_assembly_step_has_already_been_completed(self):
         metaspades_result_directory = f"{self.output_directory}/metaspades_assembly_directory/"
         if os.path.isdir(metaspades_result_directory):
-            if not os.path.exists(f"{metaspades_result_directory}/contigs.fa") or os.path.exists(f"{metaspades_result_directory}/scaffolds.fa"):
+            if not os.path.exists(f"{metaspades_result_directory}/contigs.fa") or not os.path.exists(f"{metaspades_result_directory}/scaffolds.fa"):
                 print("Found metaspades directory but on contigs or scaffolds. Error, exiting.")
                 exit()
             if self.using_scaffolds == True:
@@ -76,7 +76,7 @@ class ContigAbundances:
     def build_indices(self):
         
         indice_basename = f"{self.output_directory}/contig_indices"
-        bowtie_indice_build_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/prebinning', 'bowtie2-build', '--threads', self.threads, self.contig_file_path, indice_basename]
+        bowtie_indice_build_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/prebinning', 'bowtie2-build', '--threads', self.threads, self.contig_file_path, indice_basename]
         run_and_log_a_subprocess(self.output_directory, bowtie_indice_build_args, "bowtie_build_contig_indices")
         return indice_basename
         
@@ -84,7 +84,7 @@ class ContigAbundances:
         
         sam_output_file = f"{self.output_directory}/aligned_reads_to_contigs.sam"
         
-        bowtie2_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/prebinning', 'bowtie2', '-x', self.indice_basename, '-1', self.read_fwd_path, 
+        bowtie2_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/prebinning', 'bowtie2', '-x', self.indice_basename, '-1', self.read_fwd_path, 
                         '-2', self.read_rev_path, '-p', self.threads, '--very-sensitive', 
                         '--no-unal', '-S', sam_output_file]
         
@@ -96,9 +96,9 @@ class ContigAbundances:
     def convert_and_sort_aligned_sam_file(self):
         bam_output_file = f"{self.output_directory}/aligned_reads_to_contigs.bam"
         sorted_bam_output_file = f"{self.output_directory}/aligned_reads_to_contigs.bam"
-        samtools_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/prebinning', 'samtools', 'view', '-@', self.threads, '-Sb', self.aligned_sam_file, '-o', bam_output_file] 
-        samtools_sort_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/prebinning', 'samtools', 'sort', '-@', self.threads, '-O', 'bam', '-o', bam_output_file, sorted_bam_output_file]
-        samtools_index_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/prebinning', 'samtools', 'index', '-@', self.threads, sorted_bam_output_file]
+        samtools_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/prebinning', 'samtools', 'view', '-@', self.threads, '-Sb', self.aligned_sam_file, '-o', bam_output_file] 
+        samtools_sort_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/prebinning', 'samtools', 'sort', '-@', self.threads, '-O', 'bam', '-o', bam_output_file, sorted_bam_output_file]
+        samtools_index_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/prebinning', 'samtools', 'index', '-@', self.threads, sorted_bam_output_file]
         
         run_and_log_a_subprocess(self.output_directory, samtools_args, 'samtools_conversion')
         run_and_log_a_subprocess(self.output_directory, samtools_sort_args, 'samtools_sort')
@@ -152,14 +152,14 @@ class Binner:
     @classmethod
     def calculate_read_depth(cls, depth_output_path):
         
-        get_contig_depth_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'jgi_summarize_bam_contig_depths', '--outputDepth', depth_output_path, cls.abundance_information_path]
+        get_contig_depth_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'jgi_summarize_bam_contig_depths', '--outputDepth', depth_output_path, cls.abundance_information_path]
         run_and_log_a_subprocess(cls.log_directory_path, get_contig_depth_args, "metabat2_contig_read_depth_gen")
         cls.read_depths_path = depth_output_path
        
         
     def run_maxbin2(self, output_directory):
         
-        maxbin2_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'run_MaxBin.pl', '-contig', self.contigs_path,
+        maxbin2_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'run_MaxBin.pl', '-contig', self.contigs_path,
                         '-thread', self.threads, '-abund', self.abundance_information_path, '-out', f"{output_directory}/sample_result_"]
         
         run_and_log_a_subprocess(output_directory, maxbin2_args, "maxbin2_binning")
@@ -168,7 +168,7 @@ class Binner:
 
     def run_metabat2(self, output_directory):
         
-        metabat2_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2','metabat2', '-m', self.min_contig_length, '-t', self.threads, '--unbinned',
+        metabat2_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2','metabat2', '-m', self.min_contig_length, '-t', self.threads, '--unbinned',
                         '--seed', '0', '-i', self.contigs_path, '-a', self.read_depths_path, '-o', output_directory]
         
         run_and_log_a_subprocess(output_directory, metabat2_args, "metabat2_binning")
@@ -176,7 +176,7 @@ class Binner:
         
     def run_semibin2(self, output_directory):
         
-        semibin_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'SemiBin', 'single_easy_bin', '-i', self.contigs_path, '-b', self.abundance_information_path, '-o', output_directory, '-t', self.threads,
+        semibin_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'SemiBin', 'single_easy_bin', '-i', self.contigs_path, '-b', self.abundance_information_path, '-o', output_directory, '-t', self.threads,
                         '--write-pre-reclustering-bins', '--training-type', 'self']
         
         run_and_log_a_subprocess(output_directory, semibin_args, "semibin_binning")
@@ -184,30 +184,30 @@ class Binner:
     def run_concoct(self, output_directory):
         contig_bed_file_path = f"{output_directory}/contigs_10k.bed"
         contig_10k_file_path = f"{output_directory}/contigs_10k.fa"
-        step_1_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'cut_up_fasta.py', self.contigs_path, '-c', '10000', '-o', '0', '--merge_last', '-b', contig_bed_file_path]
+        step_1_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'cut_up_fasta.py', self.contigs_path, '-c', '10000', '-o', '0', '--merge_last', '-b', contig_bed_file_path]
         run_and_log_a_subprocess(output_directory, step_1_args, "concoct_cutup_fasta", alternate_stdout_path=contig_10k_file_path)
         
         coverage_table_path = f"{output_directory}/coverage_table.tsv"
-        step_2_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'concoct_coverage_table.py', contig_bed_file_path, self.abundance_information_path]
+        step_2_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'concoct_coverage_table.py', contig_bed_file_path, self.abundance_information_path]
         run_and_log_a_subprocess(output_directory, step_2_args, "concoct_gen_cov_table", alternate_stdout_path=coverage_table_path)
         
-        step_3_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'concoct', '--composition_file', self.contigs_path, '--length_threshold', self.min_contig_length, '--coverage_file', coverage_table_path, '-b', output_directory]
+        step_3_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'concoct', '--composition_file', self.contigs_path, '--length_threshold', self.min_contig_length, '--coverage_file', coverage_table_path, '-b', output_directory]
         run_and_log_a_subprocess(output_directory, step_3_args, "concoct_step_3_run_concoct")
         
         clustered_file_path = f"{output_directory}/clustering_gt{self.min_contig_length}.csv"
         clustering_merged_path = f"{output_directory}/clustering_merged.csv"
-        step_4_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'merge_cutup_clustering.py', clustered_file_path]
+        step_4_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'merge_cutup_clustering.py', clustered_file_path]
         run_and_log_a_subprocess(output_directory, step_4_args, "concoct_gen_cov_table", alternate_stdout_path=clustering_merged_path)
         final_concoct_bins_path = f"{output_directory}/fasta_bins/"
         os.mkdir(final_concoct_bins_path)
         
-        step_1_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'extract_fasta_bins.py', self.contigs_path, clustering_merged_path, '--output_path', final_concoct_bins_path]
+        step_1_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/CONCOCTMetabat2MaxBin2SemiBin2', 'extract_fasta_bins.py', self.contigs_path, clustering_merged_path, '--output_path', final_concoct_bins_path]
         run_and_log_a_subprocess(output_directory, step_3_args, "concoct_final_step_extract_fasta_bins")
         
         
     def run_vamb(self, output_directory):
             # vamb --outdir path/to/outdir --fasta /path/to/catalogue.fna.gz --bamfiles /path/to/bam/*.bam -o C
-        vamb_args = ['conda', 'run', '--prefix', '/opt/miniconda3/envs/Vamb4', 'vamb', '--outdir', output_directory, '--fasta', self.contigs_path, '--bamfiles', self.abundance_information_path, '-m', self.min_contig_length,
+        vamb_args = ['mamba', 'run', '--prefix', '/opt/mamba/envs/Vamb4', 'vamb', '--outdir', output_directory, '--fasta', self.contigs_path, '--bamfiles', self.abundance_information_path, '-m', self.min_contig_length,
                          '-p', self.threads]
         run_and_log_a_subprocess(output_directory, vamb_args, "vamb_binning")
 
